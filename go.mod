cf567b320a8df7d82baf866462e3bef549c159bf
Jingda Mai <datsmai@hotmail.com>
2020-08-30 15:31:49 +0900

    items service added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JingdaMai/bookstore_items-api
+
+go 1.12
+
+require github.com/JingdaMai/bookstore_utils-go v0.0.0-20200830062538-2da1845df5cc
--- services/items_service.go
+++ services/items_service.go
@@ -1,1 +1,24 @@
 package services
+
+import (
+	"github.com/JingdaMai/bookstore_items-api/domain/items"
+	"github.com/JingdaMai/bookstore_utils-go/rest_errors"
+	"net/http"
+)
+
+var ItemsService itemsServiceInterface = &itemsService{}
+
+type itemsServiceInterface interface {
+	Create(items.Item) (*items.Item, rest_errors.RestErr)
+	Get(string) (*items.Item, rest_errors.RestErr)
+}
+
+type itemsService struct{}
+
+func (s *itemsService) Create(items.Item) (*items.Item, rest_errors.RestErr) {
+	return nil, rest_errors.NewRestError("implement me", http.StatusNotImplemented, "not_implemented", nil)
+}
+
+func (s *itemsService) Get(string) (*items.Item, rest_errors.RestErr) {
+	return nil, rest_errors.NewRestError("implement me", http.StatusNotImplemented, "not_implemented", nil)
+}
